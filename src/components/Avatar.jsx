c3e5166e34e8d217f98e3f1346e70b942ba4637b
NeagaStefan <neagastfean@gmail.com>
2022-10-11 08:58:41 +0300

    v.02

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,10 @@
+import './Avatar.css'
+
+export default function Avatar() {
+    return(
+        <div className={"avatar"}>
+            <img src={} alt={"user avatar"}/>
+
+        </div>
+    )
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOs9c7rbwvT7DIPrZK475_lIN1UUXBPoM",
+    authDomain: "thedojosite-613e3.firebaseapp.com",
+    projectId: "thedojosite-613e3",
+    storageBucket: "thedojosite-613e3.appspot.com",
+    messagingSenderId: "740218918688",
+    appId: "1:740218918688:web:be6e86e3d7958f53166945"
+};
+
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+//init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
+
+//timestamp
+const  timestamp = firebase.firestore.Timestamp
+
+export { projectAuth, projectFirestore,projectStorage, timestamp  }
